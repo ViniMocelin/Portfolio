185052b96c9f0f7d314d74a88325495ab9ff62f3
ViniMocelin <vinemocelin@gmail.com>
2022-10-03 06:15:57 -0300

    iniciando contato

--- src/pages1/Contato/Contato.jsx
+++ src/pages1/Contato/Contato.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import S from './Contato.module.css'
+import TitCo from '../../assets/img/5.png'
 
 const Contato = () => {
   return (
-    <div>Contato</div>
+    <div className={S.contContato} id='Contact'>
+       <div className={S.titCo}>
+        <img className={S.cont} src={TitCo} alt="" />
+      </div>
+    </div>
   )
 }
 
