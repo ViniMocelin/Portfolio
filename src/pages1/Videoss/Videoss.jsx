329a9a2269c65a4f53dfeaa2b34f4704f3130317
ViniMocelin <vinemocelin@gmail.com>
2022-10-06 19:21:32 -0300

    titulo video

--- src/pages1/Videoss/Videoss.jsx
+++ src/pages1/Videoss/Videoss.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import S from './Videos.module.css'
+import TitVid from '../../assets/img/videos.png'
 
 const Videoss = () => {
   return (
     <div className={S.contVid} id="Videos">
+        <div className={S.titVid}>
+        <img className={S.vidvid} src={TitVid} alt="" />
+      </div>
         <div className={S.bloco1}>
         <iframe className={S.video1} width="480" height="275" src="https://www.youtube.com/embed/hyLjyPtiqjc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
         <iframe className={S.video2} width="480" height="275" src="https://www.youtube.com/embed/JIIS92FapyM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
